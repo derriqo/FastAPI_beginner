0996de9006551c9547cd2af1d087da001f46a8e2
derrrick william <williamderrick100@gmail.com>
2022-06-17 09:58:50 +0300

    Added blog limit parameter

--- main.py
+++ main.py
@@ -3,11 +3,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/blog')
 
-def index():
-    return {'message':'Hello World'}
+def index(limit,published : bool):
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+    #only get 10 published blogs
+    # return published
+    if published:
+
+        return {'message': f'{limit} published blog from the db'}
+    else:
+         return {'message': f'{limit} blog from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+
+    #fetch comments of blog with id = id
+
+    return {'data':{'1','2'}}
